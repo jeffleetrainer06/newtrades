b363e23ecf751ca589abdba51a765391c32a925e
jeffleetrainer06 <jeffleetrainer@gmail.com>
2025-08-31 14:13:20 -0600

    Create vite.config.ts
    Netlify update

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      }
+    }
+  },
+  define: {
+    __APP_VERSION__: JSON.stringify(Date.now())
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
